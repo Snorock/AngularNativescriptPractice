b083f6dc86cc4bd8c7a4b388d90c9c583cf7958b
snorock <ibrovche@gmail.com>
2018-08-06 21:47:05 -0400

    list component added

--- app/pt-backlog/pt-backlog.module.ts
+++ app/pt-backlog/pt-backlog.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 
+import {NativeScriptModule} from 'nativescript-angular/nativescript.module';
+
 import { PtBacklogComponent } from './pt-backlog.component';
 import {PipesModule} from './pipes/pipes.module'
+import {PTItemListComponent} from './pt-item-list/pt-item-list.component';
 
 @NgModule({
-    imports: [PipesModule],
+    imports: [NativeScriptModule, PipesModule],
     exports: [PtBacklogComponent],
-    declarations: [PtBacklogComponent],
+    declarations: [PtBacklogComponent,
+         PTItemListComponent],
     providers: [],
 })
 export class PtBacklogModule { }
